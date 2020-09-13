40de28103207ad046acc6054885f73e3446c62bf
StefanutVlad <stefanut.tony@yahoo.com>
2020-09-13 23:23:36 +0300

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //Will only run onche when the app component loads...
+    
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS ---- ', authUser);
+
+      
+      if(authUser) {
+        //The user just logged in / the user was logged in  
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        //The user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
     // BEM convention
     <Router>
       <div className="app">
-        <Header />
-
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,23 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAWULbA8p8K4hfsdVYA0ZwxIcXf5ysl7fw",
-    authDomain: "clone-project-f8143.firebaseapp.com",
-    databaseURL: "https://clone-project-f8143.firebaseio.com",
-    projectId: "clone-project-f8143",
-    storageBucket: "clone-project-f8143.appspot.com",
-    messagingSenderId: "878273068248",
-    appId: "1:878273068248:web:cf7ee8da4421f945de6bad",
-    measurementId: "G-FNF4LYEYE8"
-  };
+  apiKey: "AIzaSyAWULbA8p8K4hfsdVYA0ZwxIcXf5ysl7fw",
+  authDomain: "clone-project-f8143.firebaseapp.com",
+  databaseURL: "https://clone-project-f8143.firebaseio.com",
+  projectId: "clone-project-f8143",
+  storageBucket: "clone-project-f8143.appspot.com",
+  messagingSenderId: "878273068248",
+  appId: "1:878273068248:web:cf7ee8da4421f945de6bad",
+  measurementId: "G-FNF4LYEYE8",
+};
+
+//App initilization
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+//DB initilization
+const db = firebaseApp.firestore();
+
+//Authentication variable
+const auth = firebase.auth();
+
+export { db, auth };
