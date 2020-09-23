96df46958660c56fd6ccdc801debc654e8915a86
StefanutVlad <stefanut.tony@yahoo.com>
2020-09-24 02:12:48 +0300

    Deploy debugging

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //THE API (Cloud function) URL
-  baseURL: "http://localhost:5001/clone-project-f8143/us-central1/api",
-});
+  //THE API (Cloud function) URL - firebase blaze
+  baseURL: "https://us-central1-clone-project-f8143.cloudfunctions.net/api",
 
+  //local host point: http://localhost:5001/clone-project-f8143/us-central1/api
+});
+i;
 export default instance;
